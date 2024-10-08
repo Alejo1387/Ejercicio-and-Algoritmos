c46a6a6ef62958d47d951eee5b8a0b3f00d9db75
Alejo1387 <trabajoalgoritmos02@gmail.com>
2024-10-08 18:52:32 -0500

    ejercicio, readme y img

---
+++ 9_Ejercicio/Birthday_Cake_Candles.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    number1=0
+    number2=0
+    for i in range(candles_count):
+        if candles[i]>number1:
+            number1=candles[i]
+    for j in range(candles_count):
+        if candles[j]==number1:
+            number2+=1
+    return number2
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ 9_Ejercicio/README.md
@@ -0,0 +1,7 @@
+# 9 Ejercicio
+
+### Quenstion
+![imagen pegada](img2.png)
+
+### Solution
+![imagen pegada (2)](img2.png)
