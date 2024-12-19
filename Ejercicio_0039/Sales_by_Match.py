8f5b6d67646f77dc8a29da461937a04fec2328d4
Alejo1387 <trabajoalgoritmos02@gmail.com>
2024-12-19 18:02:02 -0500

    ejercicio, readme y img

---
+++ Ejercicio_0039/Sales_by_Match.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    ar.sort()
+    nn = 0
+    i=0
+    while i < len(ar)-1:
+        if ar[i] == ar[i+1]:
+            i+=2
+            nn+=1
+        else:
+            i+=1
+    return nn
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
