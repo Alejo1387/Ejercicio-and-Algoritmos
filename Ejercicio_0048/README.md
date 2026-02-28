6b7a5aa69b7b0d64e3ae5116d0a2b5a965f27443
Alejo1387 <trabajoalgoritmos02@gmail.com>
2026-02-28 13:55:23 -0500

    change exercise

--- Ejercicio_0048/README.md
+++ Ejercicio_0048/README.md
@@ -2,3 +2,8 @@
 
 ## Quetions
 ![imagen pegada](img1.png)
+
+## Solution
+
+#### Python
+![imagen pegada (2)](img2.png)
--- Ejercicio_0048/minutesBeforeYear.py
+++ Ejercicio_0048/minutesBeforeYear.py
@@ -1,8 +1,28 @@
+# def minutesBeforeYear(h, m):
+#     return ((24 - h) * 60) - m
+
+# print(minutesBeforeYear(23, 55))
+# print(minutesBeforeYear(23, 0))
+# print(minutesBeforeYear(0, 1))
+# print(minutesBeforeYear(4, 20))
+# print(minutesBeforeYear(23, 59))
+
+import sys
+
+# For single line input
+line = int(sys.stdin.readline())
+
 def minutesBeforeYear(h, m):
     return ((24 - h) * 60) - m
 
-print(minutesBeforeYear(23, 55))
-print(minutesBeforeYear(23, 0))
-print(minutesBeforeYear(0, 1))
-print(minutesBeforeYear(4, 20))
-print(minutesBeforeYear(23, 59))
+# For single integer
+# n = int(sys.stdin.readline())
+
+# For multiple integers
+# a, b = map(int, sys.stdin.readline().split())
+# minutesBeforeYear(a, b)
+
+# For list of integers
+for i in range(line):
+    h, m = map(int, sys.stdin.readline().split())
+    print(minutesBeforeYear(h, m))
